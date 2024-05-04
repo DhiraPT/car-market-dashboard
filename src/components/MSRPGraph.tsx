8d6d23a30fd5bc044f1fcb8d2e3b36ea08c9a604
Dhiraputta Pathama Tengara <dhira.pt@gmail.com>
2024-05-05 03:36:36 +0800

    Implement period change for MSRP graph

--- src/components/MSRPGraph.tsx
+++ src/components/MSRPGraph.tsx
@@ -1,13 +1,17 @@
 import { Line, LineConfig } from "@ant-design/charts";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { DataContext } from "../providers/DataContextProvider";
 import { Segmented } from "antd";
 import { formatDate } from "../utils/date.utils";
 
 const MSRPGraph: React.FC = () => {
   const { selectedCarData, checkedKeys, coeData } = useContext(DataContext);
+  const [msrpData, setMsrpData] = useState<
+    { key: string; label: string; date: string; msrp: number; coe_type: string | null }[]
+  >([]);
+  const [config, setConfig] = useState<LineConfig>({});
 
-  const msrpData = selectedCarData.flatMap((carModel) => {
+  const initMsrpData = selectedCarData.flatMap((carModel) => {
     return carModel.CarSubmodels.flatMap((carSubmodel) => {
       return carSubmodel.CarPrices.map((carPrice) => {
         return {
@@ -21,13 +25,13 @@ const MSRPGraph: React.FC = () => {
     });
   });
 
-  // Earliest date in the msrpData
+  // Earliest date in the MSRP data
   const earliestDate = new Date(
-    Math.min(...msrpData.map((entry) => new Date(entry.date).getTime())),
+    Math.min(...initMsrpData.map((entry) => new Date(entry.date).getTime())),
   );
 
   // COE categories selected
-  const coeCategories = Array.from(new Set(msrpData.map((entry) => entry.coe_type)));
+  const coeCategories = Array.from(new Set(initMsrpData.map((entry) => entry.coe_type)));
 
   // Filter COE data based on the earliest date. Else, return all COE data if no car models are selected
   const filteredCoeData = coeData.filter(
@@ -36,8 +40,8 @@ const MSRPGraph: React.FC = () => {
       checkedKeys.length === 0,
   );
 
-  // Concatenate the filtered COE data with msrpData
-  msrpData.push(
+  // Concatenate the filtered COE data with MSRP data
+  initMsrpData.push(
     ...filteredCoeData.map((coe) => {
       return {
         key: `COE Category ${coe.coe_type}`,
@@ -48,37 +52,64 @@ const MSRPGraph: React.FC = () => {
       };
     }),
   );
+  
+  initMsrpData.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
 
-  msrpData.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+  useEffect(() => {
+    setMsrpData(initMsrpData);
+  }, [selectedCarData]);
 
-  const config: LineConfig = {
-    title: {
-      title: "MSRP Over Time",
-      style: {
-        align: "center",
-        titleFontSize: 36,
+  useEffect(() => {
+    setConfig({
+      title: {
+        title: "MSRP Over Time",
+        style: {
+          align: "center",
+          titleFontSize: 36,
+        },
       },
-    },
-    height: 500,
-    forceFit: true,
-    padding: "auto",
-    data: msrpData,
-    xField: "date",
-    yField: "msrp",
-    colorField: "label",
-    slider: {
-      x: {
-        values: [0.6, 1],
+      height: 500,
+      forceFit: true,
+      padding: "auto",
+      data: msrpData,
+      xField: "date",
+      yField: "msrp",
+      colorField: "label",
+      slider: {
+        x: {
+          values: [0, 1],
+        },
       },
-    },
-    responsive: true,
-    interaction: {
-      legendFilter: false,
-    },
-  };
+      responsive: true,
+      interaction: {
+        legendFilter: false,
+      },
+    });
+  }, [msrpData]);
 
   const changePeriod = (value: string) => {
-    console.log(value);
+    const currentDate = new Date();
+    const sixMonthsAgo = new Date(currentDate.setMonth(currentDate.getMonth() - 6));
+    const twelveMonthsAgo = new Date(currentDate.setMonth(currentDate.getMonth() - 6));
+    const eighteenMonthsAgo = new Date(currentDate.setMonth(currentDate.getMonth() - 6));
+    const twentyFourMonthsAgo = new Date(currentDate.setMonth(currentDate.getMonth() - 6));
+
+    const filteredMsrpData = initMsrpData.filter((entry) => {
+      const date = new Date(entry.date);
+      switch (value) {
+        case "6 Months":
+          return date >= sixMonthsAgo;
+        case "12 Months":
+          return date >= twelveMonthsAgo;
+        case "18 Months":
+          return date >= eighteenMonthsAgo;
+        case "24 Months":
+          return date >= twentyFourMonthsAgo;
+        default:
+          return true;
+      }
+    });
+    setMsrpData(filteredMsrpData);
   };
 
   return (
